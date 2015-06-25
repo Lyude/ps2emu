d5ed854cb566cb53c87eb82794b9aa6c4795244a
Stephen Chandler Paul <thatslyude@gmail.com>
2015-06-25 10:15:31 -0400

    autogen.sh: Initialize git submodules

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ "$1" != "--no-submodule-init" ]; then
+    git submodule init
+    git submodule update
+fi
+
 [ -d "build-aux" ] || mkdir build-aux
 autoreconf --install -v --force
 ./configure $@
